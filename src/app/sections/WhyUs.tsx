575286d94ce243d31d5bf9672bbe8b7ced2eadc1
freetimezp <kraftukrnet@ukr.net>
2024-04-23 13:17:48 +0300

    01 deploy test - eatme-13

--- src/app/sections/WhyUs.tsx
+++ src/app/sections/WhyUs.tsx
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './whyUs.css';
 
 import WhyUsCard from '../components/WhyUsCard';
 import SectionTitle from '../components/SectionTitle';
 
-async function getWhyUsData() {
-    const res = await fetch('http://localhost:3000/api/whyus');
-    return res.json();
-}
+// async function getWhyUsData() {
+//     const res = await fetch('http://localhost:3000/api/whyus');
+//     return res.json();
+// }
 
-export default async function WhyUs() {
-    const items: [] = await getWhyUsData();
+const whyUs = [
+    {
+        id: 1,
+        title: 'Lorem Ipsum',
+        content: `Ulamco laboris nisi ut aliquip ex ea commodo consequat. Et
+          consectetur ducimus vero placeat`,
+    },
+    {
+        id: 2,
+        title: 'Repellat Nihil',
+        content: `Dolorem est fugiat occaecati voluptate velit esse. Dicta
+      veritatis dolor quod et vel dire leno para dest`,
+    },
+    {
+        id: 3,
+        title: 'Ad ad velit qui',
+        content: `Molestiae officiis omnis illo asperiores. Aut doloribus vitae
+      sunt debitis quo vel nam quis`,
+    },
+];
+
+
+export default function WhyUs() {
+    const [items, setItems] = useState<[] | any>([]);
+
+    useEffect(() => {
+        setItems(whyUs);
+    }, []);
 
     return (
         <section id="why-us" className='why-us'>
