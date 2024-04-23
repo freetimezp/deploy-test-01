ea6f31539ba2a985c89167ccc82dc172f9ffbcae
freetimezp <kraftukrnet@ukr.net>
2024-04-23 12:20:52 +0300

    01 deploy test - eatme-13

---
+++ src/app/page.tsx
@@ -0,0 +1,31 @@
+import Hero from "./sections/Hero";
+// import About from "./sections/About";
+// import WhyUs from "./sections/WhyUs";
+// import Menu from "./sections/Menu";
+// import Specials from "./sections/Specials";
+// import Events from "./sections/Events";
+// import Booking from "./sections/Booking";
+// import Testimonials from "./sections/Testimonials";
+// import Gallery from "./sections/Gallery";
+// import Chefs from "./sections/Chefs";
+// import Contact from "./sections/Contact";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      {/* <main id="main">
+        <About />
+        <WhyUs />
+        <Menu />
+        <Specials />
+        <Events />
+        <Booking />
+        <Testimonials />
+        <Gallery />
+        <Chefs />
+        <Contact />
+      </main> */}
+    </>
+  );
+}
